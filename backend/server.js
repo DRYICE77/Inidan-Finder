15ca3b4c4bdff05a248dd447e1c52d1f8d64fa15
DRYICE77 <91396930+DRYICE77@users.noreply.github.com>
2025-12-12 10:33:11 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+try { require("dotenv").config(); } catch (e) {}
 const express = require("express");
 const session = require("express-session");
 const cookieParser = require("cookie-parser");
